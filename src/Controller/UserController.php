08e88aeeaf20a696438c23e23b882f0217fdeb04
Perrine96 <perrinemallard96@gmail.com>
2024-11-07 17:17:57 +0100

    ajout bdd

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CharactersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,22 +10,23 @@ use Symfony\Component\Routing\Attribute\Route;
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'user')]
-    public function index(): Response
+    public function index(CharactersRepository $charactersRepository): Response
     {
-        $users = [
-            ['name' => 'Luna', 'xp' => 25, 'type' => 'data whisperer', 'gender' => 'female', 'role' => 'Hacker and intelligence gatherer', 'category' => 'augmented human'],
-            ['name' => 'Kade', 'xp' => 152, 'type' => 'tech guru', 'gender' => 'male', 'role' => 'Engineer and tech specialist', 'category' => 'augmented human'], 
-            ['name' => 'Raine', 'xp' => 247, 'type' => 'synth medic', 'gender' => 'non-binary', 'role' => 'Healer and support', 'category' => 'human'], 
-            ['name' => 'Zephyr', 'xp' => 789, 'type' => 'corrupted overlord', 'gender' => 'male', 'role' => 'Main antagonist and power-hungry ruler', 'category' => 'human'],
-            ['name' => 'Nova', 'xp' => 593, 'type' => 'glitch artificer', 'gender' => 'female', 'role' => 'Engineer and tech specialist', 'category' => 'augmented human'],
-            ['name' => 'Orion', 'xp' => 143, 'type' => 'circuit prophet', 'gender' => 'non-binary', 'role' => 'Visionary and strategist', 'category' => 'human'],
-            ['name' => 'Jax', 'xp' => 435, 'type' => 'echo runner', 'gender' => 'male', 'role' => 'Scout and speedster', 'category' => 'augmented human'],
-            ['name' => 'Lyra', 'xp' => 143, 'type' => 'neuro ghost', 'gender' => 'female', 'role' => 'Cyber psychic and manipulator', 'category' => 'human'],
-            ['name' => 'Draven', 'xp' => 143, 'type' => 'cyber oracle', 'gender' => 'male', 'role' => 'Strategist and foresight expert', 'category' => 'augmented human'],
-        ];
+        $characters = $charactersRepository->findAll();
+        // $users = [
+        //     ['name' => 'Luna', 'xp' => 25, 'type' => 'data whisperer', 'gender' => 'female', 'role' => 'Hacker and intelligence gatherer', 'category' => 'augmented human'],
+        //     ['name' => 'Kade', 'xp' => 152, 'type' => 'tech guru', 'gender' => 'male', 'role' => 'Engineer and tech specialist', 'category' => 'augmented human'], 
+        //     ['name' => 'Raine', 'xp' => 247, 'type' => 'synth medic', 'gender' => 'non-binary', 'role' => 'Healer and support', 'category' => 'human'], 
+        //     ['name' => 'Zephyr', 'xp' => 789, 'type' => 'corrupted overlord', 'gender' => 'male', 'role' => 'Main antagonist and power-hungry ruler', 'category' => 'human'],
+        //     ['name' => 'Nova', 'xp' => 593, 'type' => 'glitch artificer', 'gender' => 'female', 'role' => 'Engineer and tech specialist', 'category' => 'augmented human'],
+        //     ['name' => 'Orion', 'xp' => 143, 'type' => 'circuit prophet', 'gender' => 'non-binary', 'role' => 'Visionary and strategist', 'category' => 'human'],
+        //     ['name' => 'Jax', 'xp' => 435, 'type' => 'echo runner', 'gender' => 'male', 'role' => 'Scout and speedster', 'category' => 'augmented human'],
+        //     ['name' => 'Lyra', 'xp' => 143, 'type' => 'neuro ghost', 'gender' => 'female', 'role' => 'Cyber psychic and manipulator', 'category' => 'human'],
+        //     ['name' => 'Draven', 'xp' => 143, 'type' => 'cyber oracle', 'gender' => 'male', 'role' => 'Strategist and foresight expert', 'category' => 'augmented human'],
+        // ];
 
         return $this->render('user/user.html.twig', [
-            'users' => $users,
+            'users' => $characters,
         ]);
     }
 }
